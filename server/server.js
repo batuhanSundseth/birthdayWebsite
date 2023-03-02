8001243fd107e19734ec1f737a0ec497e7eb668b
batuhanSundseth <batuhansundseth@pm.me>
2023-03-02 15:45:35 -0500

    Made server send the correct files

--- server/server.js
+++ server/server.js
@@ -9,15 +9,15 @@ app.use(express.static('client'))
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+  res.status(200).sendFile(path.join(__dirname, '../client/birthdayWebsite.html'))
 })
 
 app.get('/css', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/styles.css'))
+  res.status(200).sendFile(path.join(__dirname, '../client/birthdayWebsite.css'))
 })
 
 app.get('/js', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/main.js'))
+  res.status(200).sendFile(path.join(__dirname, '../client/birthdayWebsitejs'))
 })
 
 app.listen(4000, console.log("App running on 4000"))
