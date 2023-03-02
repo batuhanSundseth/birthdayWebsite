3ff6569289c4657a7e1c9cdd27f808c9cc0199a2
batuhanSundseth <batuhansundseth@pm.me>
2023-03-02 16:09:06 -0500

    Added rollbar object

--- server/server.js
+++ server/server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '787820224b794da6b4d94c21986427f3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.use(express.json())
